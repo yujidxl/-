c9a904a8c97c845113d3e36c6a20f2803b6f4707
yujjdxl <1435409156@qq.com>
2018-04-28 00:36:07 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div class="home">
+      Hello World
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
