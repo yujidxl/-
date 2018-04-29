1c7bc7013ded77ce8f698d49ceb6a14500b0853d
yujjdxl <1435409156@qq.com>
2018-04-29 21:30:03 +0800

    master

---
+++ src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <city-header></city-header>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
