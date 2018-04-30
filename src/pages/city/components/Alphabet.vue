f0f851d84aa5870246168b116bca66e713cb9031
yujjdxl <1435409156@qq.com>
2018-04-30 16:14:05 +0800

    git add ajax

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,19 +1,20 @@
 <template lang="html">
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item"
+      v-for="(item,key) of cities"
+      :key="key"
+    >
+    {{key}}
+    </li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'CityAlphabet'
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
