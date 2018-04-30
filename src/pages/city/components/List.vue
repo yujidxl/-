7ce5d02fca1f6fa6f0d388d55b7f562e0040c5e3
yujjdxl <1435409156@qq.com>
2018-04-30 18:30:38 +0800

    add-city-components

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -23,6 +23,7 @@
     <div class="area"
       v-for="(item,key) of cities"
       :key="key"
+      :ref="key"
     >
       <div class="title border-topbottom">{{key}}</div>
       <div class="item-list">
@@ -42,11 +43,20 @@ import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
   props: {
+    letter: String,
     hot: Array,
     cities: Object
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
